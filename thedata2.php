b5d61b2aeb1c0b6799eb324e793072d7fa71720a
sevold <30608605+sevold@users.noreply.github.com>
2021-03-01 11:54:23 -0400

    Add files via upload
    php files for collecting and storing data received from water quality monitoring station

---
+++ thedata2.php
@@ -0,0 +1,19 @@
+<?php
+include('database.php');
+$ip = $_SERVER['REMOTE_ADDR'];
+$unitid  = $_GET['unitid'];
+$temp  = $_GET['temp'];
+$ph  = $_GET['ph'];
+$do  = $_GET['do'];
+
+$thetimestamp=strtotime("now");
+$posteddate=date("Y-m-d");
+date_default_timezone_set('Jamaica');
+$dat1=date('h:');
+$dat=date('i:s A');
+$dat2=$dat1+1;
+$otherdate=$dat2.":". $dat;
+$registered=$posteddate ." ".$dat2.":". $dat;
+
+mysqli_query($con,"INSERT INTO waterquality VALUES (NULL,'$unitid','$temp','$ph','$do','$ip', '$thetimestamp', '$registered','$posteddate','$otherdate')" );
+?>
